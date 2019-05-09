205a9cc3ecfb8e6846688d6ac5faaf7037bfc05d
Mahesh Budi <mahesh.budi@tr.com>
2019-05-09 13:58:49 +0000

    added publicIp details to aws-list.py

--- aws-list.py
+++ aws-list.py
@@ -7,10 +7,12 @@ di=ec2.describe_instances()['Reservations']
 
 for inst in range(len(di)):
     inst_id=di[inst]['Instances'][0]['InstanceId']
-    if not 'Tags' in di[inst]['Instances'][0]: inst_name = 'None'
-    else: inst_name=di[inst]['Instances'][0]['Tags'][0]['Value']
+    inst_name = 'None'
+    if 'Tags' in di[inst]['Instances'][0]: 
+        inst_name=di[inst]['Instances'][0]['Tags'][0]['Value']
     inst_status=di[inst]['Instances'][0]['State']['Name']
-    print("%s - %s - %s" % (inst_id,inst_name,inst_status))
-
+    inst_pub_ip = 'None'
+    if 'PublicIpAddress' in di[inst]['Instances'][0]: inst_pub_ip=di[inst]['Instances'][0]['PublicIpAddress']
+    print("%s - %s - %s - %s" % (inst_id,inst_name,inst_status,inst_pub_ip))
 
 
